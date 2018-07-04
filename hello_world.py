7c3650fe4bc1897e5b668f7ab6a409ba1ab066c6
Vince <vincentfiorilli@gmail.com>
2018-07-03 20:54:43 -0400

    up through lists

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,23 @@
 print('hello python world!')
+
+name = 'Vince fiorilli'
+print(name.title() + '!')
+
+nameList = ['Vince', 'Chris', 'Carol', 'Joe']
+
+#print(nameList[0])
+#print(nameList[1])
+nameList[0] = 'Superman'
+print(nameList)
+
+cantCome = 'Chris'
+nameList.remove(cantCome)
+print(nameList)
+
+nameList.insert(2, 'cookoo')
+print(nameList)
+
+nameList.pop()
+print(nameList)
+
+print('length ' + str(len(nameList)))
