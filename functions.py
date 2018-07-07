e3a57ad317a684d65eef8af3c675f32da7f0690f
Vince <vincentfiorilli@gmail.com>
2018-07-07 08:37:20 -0400

    function practice

---
+++ functions.py
@@ -0,0 +1,10 @@
+def display_message():
+    print('we\'re learning python functions!')
+
+display_message()
+
+def favorite_book(title):
+    #book = input('What\'s youre favorite book?')
+    print('your favorite book is ' + title)
+
+favorite_book('alice in wonderland')
