3f0abef378f2b4782e10d33ec5cc3a93be1455be
Vince <vincentfiorilli@gmail.com>
2018-07-07 10:23:34 -0400

    pg 166 text excersice

--- class.py
+++ class.py
@@ -11,8 +11,31 @@ class Restaurant():
 
 
 #make an instance of the class
-restaurant = Restaurant('bitches at work', 'organic')
+restaurant1 = Restaurant('bitches at work', 'organic')
+restaurant2 = Restaurant('coders bay', 'tough food')
+restaurant3 = Restaurant('fools good', 'carribean')
 
+'''
 print(restaurant.restaurant_name + ' ' + restaurant.cuisine_type)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+'''
+
+'''
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
+restaurant3.describe_restaurant()
+'''
+class User():
+    def __init__(self, first_name, last_name, interest):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.interest = interest
+    def describe_user(self):
+        print('First name is ' + self.first_name)
+        print('Last name is ' + self.last_name)
+        print('User interest is ' + self.interest)
+
+vince = User('Vince', 'Fiorilli', 'coding')
+
+vince.describe_user()
